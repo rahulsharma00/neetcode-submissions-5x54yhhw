f8be3acbc29e1824df4eb8d47e7b74e5d9e27efc
Rahul Sharma <89294054+rahulsharma00@users.noreply.github.com>
2026-08-02 18:11:34 +0530

    Add: design-hashset - submission-8

---
+++ Data Structures & Algorithms/design-hashset/submission-8.py
@@ -0,0 +1,43 @@
+class LinkedList:
+    def __init__(self,val):
+        self.val = val
+        self.next = None 
+
+class MyHashSet:
+
+    def __init__(self):
+        self.arr = [LinkedList(0) for i in range(10**4)]
+        
+
+    def add(self, key: int) -> None:
+        curr = self.arr[key % len(self.arr)]
+        while curr.next:
+            if curr.next.val == key:
+                return 
+            curr = curr.next 
+        curr.next = LinkedList(key)
+        
+    def remove(self, key: int) -> None:
+        curr = self.arr[key % len(self.arr)]
+        while curr.next:
+            if curr.next.val == key:
+                curr.next = curr.next.next 
+                return
+            curr = curr.next 
+
+    def contains(self, key: int) -> bool:
+        curr = self.arr[key % len(self.arr)]
+        while curr.next:
+            if curr.next.val == key:
+                return True 
+            curr = curr.next 
+        return False 
+
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
